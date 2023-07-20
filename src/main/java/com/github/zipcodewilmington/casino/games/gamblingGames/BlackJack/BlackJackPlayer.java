e45fe9791dc3011b15f396286f8558f8bccbfc77
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-07-20 19:45:35 -0400

    test

--- src/main/java/com/github/zipcodewilmington/casino/games/gamblingGames/BlackJack/BlackJackPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/gamblingGames/BlackJack/BlackJackPlayer.java
@@ -5,14 +5,12 @@ import com.github.zipcodewilmington.casino.PlayerInterface;
 import com.github.zipcodewilmington.utils.AnsiColor;
 import com.github.zipcodewilmington.utils.Card;
 import com.github.zipcodewilmington.utils.IOConsole;
-import sun.tools.jconsole.JConsole;
+
 
 import java.util.List;
 
 public class BlackJackPlayer implements PlayerInterface {
-    private List<Card> hand;
-    private int balance;
-    IOConsole console = new IOConsole(AnsiColor.YELLOW);
+
 
     @Override
     public CasinoAccount getArcadeAccount() {
@@ -23,15 +21,4 @@ public class BlackJackPlayer implements PlayerInterface {
     public <SomeReturnType> SomeReturnType play() {
         return null;
     }
-
-    public void hit(){
-        hand.add(Deck.get(0));
-    }
-
-    public int placeBet(){
-        int bet = console.getIntegerInput("Enter bet amount : ");
-        balance -= bet;
-        return bet;
-    }
-
 }
