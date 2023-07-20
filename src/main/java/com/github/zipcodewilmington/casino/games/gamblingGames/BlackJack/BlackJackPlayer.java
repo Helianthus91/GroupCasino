b82549b5a211eae4bd2744decb57897c278904d6
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-07-20 17:08:49 -0400

    started backjack classes and tests

--- src/main/java/com/github/zipcodewilmington/casino/games/gamblingGames/BlackJack/BlackJackPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/gamblingGames/BlackJack/BlackJackPlayer.java
@@ -1,4 +1,37 @@
 package com.github.zipcodewilmington.casino.games.gamblingGames.BlackJack;
 
-public class BlackJackPlayer {
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.utils.AnsiColor;
+import com.github.zipcodewilmington.utils.Card;
+import com.github.zipcodewilmington.utils.IOConsole;
+import sun.tools.jconsole.JConsole;
+
+import java.util.List;
+
+public class BlackJackPlayer implements PlayerInterface {
+    private List<Card> hand;
+    private int balance;
+    IOConsole console = new IOConsole(AnsiColor.YELLOW);
+
+    @Override
+    public CasinoAccount getArcadeAccount() {
+        return null;
+    }
+
+    @Override
+    public <SomeReturnType> SomeReturnType play() {
+        return null;
+    }
+
+    public void hit(){
+        hand.add(Deck.get(0));
+    }
+
+    public int placeBet(){
+        int bet = console.getIntegerInput("Enter bet amount : ");
+        balance -= bet;
+        return bet;
+    }
+
 }
