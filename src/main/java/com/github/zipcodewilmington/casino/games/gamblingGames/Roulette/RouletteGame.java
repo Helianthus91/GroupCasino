a582912761f7d7dad5ba9b0b82f312b71cf781d2
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-07-20 20:10:50 -0400

    update with Nigel rouletteGame

--- src/main/java/com/github/zipcodewilmington/casino/games/gamblingGames/Roulette/RouletteGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/gamblingGames/Roulette/RouletteGame.java
@@ -1,4 +1,40 @@
 package com.github.zipcodewilmington.casino.games.gamblingGames.Roulette;
-
+import java.util.Scanner;
+import java.util.Random;
 public class RouletteGame {
+    public static void main(String[] args) {
+        new RouletteGame().run();
+    }
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+    int balance = 1000;
+    int betAmount;
+    public void run() {
+        int spin = random.nextInt(37);
+        System.out.print("WELCOME TO ROULETTE!\n");
+        System.out.print("YOU HAVE $" + balance + " TO GAMBLE!\n");
+        while (balance > 0){
+            System.out.print("\nPLACE BET!");
+            betAmount= scanner.nextInt();
+            if (betAmount == 0) {System.out.println("THANK YOU FOR PLAYING! SPEND ALL YOUR MONEY!!!!" );
+                break;}
+            if (betAmount > balance) {
+                System.out.println("Relax, YOU DON'T HAVE ENOUGH MONEY! HIT THE ATM ASAP!");
+                continue;}
+            System.out.println("WHATS YOUR PREDICTION? (odd/even):");
+            String prediction = scanner.next().toLowerCase();
+            if (spin % 2 == 0 && prediction.equalsIgnoreCase("even")) {
+                System.out.println("GREAT GUESS! YOU WON" + betAmount + " DOLLARS!");
+                balance += betAmount;}
+            else if (spin % 2 != 0 && prediction.equalsIgnoreCase("odd")) {
+                System.out.println("GREAT GUESS! YOU WON" + betAmount + " DOLLARS!");
+                balance += betAmount;}
+            else {balance -= betAmount;
+                System.out.println("SORRY MAYBE NEXT TIME! YOU LOST " + betAmount + " DOLLARS");
+            }
+        }
+        System.out.println("\nTHANK YOU 4 YOUR DONATION!!");
+        System.out.println("YOU FINISHED WITH $" + balance);
+        scanner.close();
+    }
 }
