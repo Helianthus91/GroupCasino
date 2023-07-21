3a3a6ec5be4e24a920ffc1b9c82fc46963693c4d
maggie <snydermag@gmail.com>
2023-07-21 09:22:11 -0400

    updated arcade dashboard display

--- src/main/java/com/github/zipcodewilmington/Casino.java
+++ src/main/java/com/github/zipcodewilmington/Casino.java
@@ -67,7 +67,7 @@ public class Casino implements Runnable {
         return console.getStringInput(new StringBuilder()
                 .append("Welcome to the Arcade Dashboard!")
                 .append("\nFrom here, you can select any of the following options:")
-                .append("\n\t[ create-account ], [ select-game ]")
+                .append("\n\t[ create-account ], [ select-game ], [ logout ]")
                 .toString());
     }
 
