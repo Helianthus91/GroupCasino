8cba59ea3f7f209e665e580f0929f4158d558509
maggie <snydermag@gmail.com>
2023-07-25 08:45:39 -0400

    Added tests

--- src/test/java/com/github/zipcodewilmington/rockpaperscissorstest/RockPaperScissorsPlayerTest.java
+++ src/test/java/com/github/zipcodewilmington/rockpaperscissorstest/RockPaperScissorsPlayerTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class RockPaperScissorsPlayerTest {
     @Test
-    public void setBalanceTest(){
+    public void setBalanceTest1(){
         // Given
         int expected = 1000;
 
@@ -21,13 +21,43 @@ public class RockPaperScissorsPlayerTest {
     }
 
     @Test
-    public void getBalanceTest(){
+    public void setBalanceTest2(){
+        // Given
+        int expected = 4;
+
+        // When
+        RockPaperScissorsPlayer player = new RockPaperScissorsPlayer();
+        player.setBalance(4);
+
+        int actual = player.getBalance();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getBalanceTest1(){
         // Given
         int expected = 1000;
 
         // When
         RockPaperScissorsPlayer player = new RockPaperScissorsPlayer();
         player.setBalance(1000);
+
+        int actual = player.getBalance();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getBalanceTest2(){
+        // Given
+        int expected = 4;
+
+        // When
+        RockPaperScissorsPlayer player = new RockPaperScissorsPlayer();
+        player.setBalance(4);
 
         int actual = player.getBalance();
 
