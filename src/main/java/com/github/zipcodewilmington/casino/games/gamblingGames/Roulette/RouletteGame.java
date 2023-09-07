9bec01b687d0d243074b98876fd917198cd999a6
NigelBynum <136828778+NigelBynum@users.noreply.github.com>
2023-09-06 20:13:15 -0400

    UPDATED RouletteGame

--- src/main/java/com/github/zipcodewilmington/casino/games/gamblingGames/Roulette/RouletteGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/gamblingGames/Roulette/RouletteGame.java
@@ -1,18 +1,21 @@
 package com.github.zipcodewilmington.casino.games.gamblingGames.Roulette;
-import com.github.zipcodewilmington.casino.CasinoAccount;
+
 import com.github.zipcodewilmington.casino.games.gamblingGames.GamblingGame;
 
 import java.util.Scanner;
 import java.util.Random;
+
 public class RouletteGame extends GamblingGame {
     public static void main(String[] args) {
         new RouletteGame().run();
     }
+
     private Scanner scanner = new Scanner(System.in);
     private Random random = new Random();
     public int balance = 1000;
     public int betAmount;
 
+    private int spinResult; // Store the spin result here
 
     public void run() {
         Intro();
@@ -25,43 +28,50 @@ public class RouletteGame extends GamblingGame {
             if (betAmount > balance) {
                 System.out.println("Relax, YOU DON'T HAVE ENOUGH MONEY! HIT THE ATM ASAP!");
                 continue;
-
             }
             String prediction = askForPrediction();
-            performSpin();
+            performSpin(); // Perform the spin once
             evaluateResult(prediction);
+            System.out.println("CURRENT BALANCE: $" + balance); // Show balance after each spin
         }
         outro();
     }
-    public void outro(){
+
+    public void outro() {
         System.out.println("\nTHANK YOU 4 YOUR DONATION!!");
         System.out.println("YOU FINISHED WITH $" + balance);
         scanner.close();
     }
-    public void Intro(){
+
+    public void Intro() {
         System.out.print("WELCOME TO ROULETTE!\n");
         System.out.print("YOU HAVE $" + balance + " TO GAMBLE!\n");
+        System.out.print("ENTER 0 AT ANYTIME TO CASH OUT!\n");
     }
+
     public void placeBet() {
         System.out.print("\nPLACE BET!");
         betAmount = scanner.nextInt();
     }
+
     public String askForPrediction() {
-        System.out.println("WHATS YOUR PREDICTION? (odd/even):");
+        System.out.println("WHAT'S YOUR PREDICTION? (odd/even):");
         return scanner.next().toLowerCase();
     }
+
     public void performSpin() {
-        int spin = random.nextInt(37);
-        System.out.println("SPIN RESULT: " + spin);
+        spinResult = random.nextInt(37); // Store the spin result
     }
+
     public void evaluateResult(String prediction) {
         if (betAmount == 0) {
             System.out.println("THANK YOU FOR PLAYING! SPEND ALL YOUR MONEY!!!!");
             return;
         }
-        int spin = random.nextInt(37);
-        System.out.println("SPIN RESULT: " + spin);
-        if ((spin % 2 == 0 && prediction.equalsIgnoreCase("even")) || (spin % 2 != 0 && prediction.equalsIgnoreCase("odd"))) {
+
+        System.out.println("SPIN RESULT: " + spinResult); // Use the stored spin result
+
+        if ((spinResult % 2 == 0 && prediction.equalsIgnoreCase("even")) || (spinResult % 2 != 0 && prediction.equalsIgnoreCase("odd"))) {
             System.out.println("GREAT GUESS! YOU WON $" + betAmount + " DOLLARS!");
             balance += betAmount;
         } else {
@@ -69,22 +79,27 @@ public class RouletteGame extends GamblingGame {
             System.out.println("SORRY MAYBE NEXT TIME! YOU LOST $" + betAmount + " DOLLARS");
         }
     }
+
     @Override
     public void intro() {
     }
+
     @Override
     public boolean winCheck() {
         return false;
     }
+
     @Override
     public void quitAsk() {
 
     }
+
     @Override
     public int playerBet() {
         return 0;
 
     }
+
     @Override
     public void updateBalance() {
 
